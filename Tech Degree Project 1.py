b5e87ec7370a71cc5b4d2e8901bb46074b879c39
Shane Smith <72278992+Shanesmith1112@users.noreply.github.com>
2020-11-18 19:27:53 -0500

    FINAL

--- Tech Degree Project 1.py
+++ Tech Degree Project 1.py
@@ -38,7 +38,7 @@ def start_game():
                     attempts += 1
             	elif guess < 1 or guess > 10:
                     guess = int(input("\nERROR!\nThe number is between 1 and 10!\nThe number you guessed was not a valid number! "))		
-	if attempts > 1:
+	    if attempts > 1:
             print("Well Done!!\nIt only took you {} attempts to guess correctly!".format(attempts))
         else:
             print("\nYou got LUCKY, you guessed it on the first try!")
