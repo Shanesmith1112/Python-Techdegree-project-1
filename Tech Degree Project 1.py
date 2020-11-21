2573ef2fbd28a82441883b88fefef7b5542deba2
Shane Smith <72278992+Shanesmith1112@users.noreply.github.com>
2020-11-20 23:11:46 -0500

    Final

--- Tech Degree Project 1.py
+++ Tech Degree Project 1.py
@@ -1,7 +1,7 @@
 import random
 
-print(
-    "-----------------------------------\n Welcome to the Number Guessing Game! \n-----------------------------------")
+
+print("-----------------------------------\n Welcome to the Number Guessing Game! \n-----------------------------------")
 
 name = input("\nWhat is your name?  ")
 
@@ -19,7 +19,9 @@ def start_game():
             print("\nThank you for playing your best score was {}!".format(min(highscore)))
             exit()
 
+
     attempts = 0
+
     num = random.randint(1, 10)
 
     print("\nGood luck, {}!\nI'm thinking of a number between 1 and 10. Can you guess it?".format(name))
@@ -28,6 +30,7 @@ def start_game():
         try:
             guess = int(input("\nChoose a number between 1 and 10. "))
             attempts += 1
+
             while guess != num:
                 if guess < 1 or guess > 10:
                     guess = int(input("\nERROR!\nPlease enter a number between 1 and 10 "))
@@ -37,6 +40,7 @@ def start_game():
                 elif guess > num:
                     guess = int(input("\nTry Again!\nThe number is lower! "))
                     attempts += 1
+
         except ValueError:
             print("\nERROR!\nPlease enter a valid number... ")
             continue
